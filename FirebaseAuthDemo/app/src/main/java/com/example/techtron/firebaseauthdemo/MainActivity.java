8d8f0ab4002d70a3e918c4e2c5a5010cee81b86e
Phillip <phillipables1@gmail.com>
2018-03-15 23:16:42 -0400

    declared variables in main activity java class

--- FirebaseAuthDemo/app/src/main/java/com/example/techtron/firebaseauthdemo/MainActivity.java
+++ FirebaseAuthDemo/app/src/main/java/com/example/techtron/firebaseauthdemo/MainActivity.java
@@ -2,8 +2,16 @@ package com.example.techtron.firebaseauthdemo;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
+    private Button buttonRegister;
+    private EditText editTextEmail;
+    private EditText editTextPassword;
+    private TextView textViewSignup;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
