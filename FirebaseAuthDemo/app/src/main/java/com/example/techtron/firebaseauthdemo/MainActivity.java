c0dbad7944937f2ca7fac20e5dd3223bc33722e6
Phillip <phillipables1@gmail.com>
2018-03-16 19:14:14 -0400

    implement onclicklistener in main activity class

--- FirebaseAuthDemo/app/src/main/java/com/example/techtron/firebaseauthdemo/MainActivity.java
+++ FirebaseAuthDemo/app/src/main/java/com/example/techtron/firebaseauthdemo/MainActivity.java
@@ -2,18 +2,19 @@ package com.example.techtron.firebaseauthdemo;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
 
 import org.w3c.dom.Text;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements View.OnClickListener{
 
     private Button buttonRegister;
     private EditText editTextEmail;
     private EditText editTextPassword;
-    private TextView textViewSignup;
+    private TextView textViewSignin;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -21,8 +22,10 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         buttonRegister = (Button) findViewById(R.id.buttonRegister);
+
         editTextEmail = (EditText) findViewById(R.id.editTextEmail);
         editTextPassword = (EditText) findViewById(R.id.editTextPassword);
-        textViewSignup = (TextView) findViewById(R.id.textViewSignin);
+
+        textViewSignin = (TextView) findViewById(R.id.textViewSignin);
     }
 }
