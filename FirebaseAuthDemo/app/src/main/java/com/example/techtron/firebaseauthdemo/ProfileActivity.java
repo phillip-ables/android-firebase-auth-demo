b3bd3d66d17fff4da94153f45a28ec2c3bfb51b7
Phillip <phillipables1@gmail.com>
2018-03-19 15:21:24 -0400

    ProfileActivity declare and initialize variables

--- FirebaseAuthDemo/app/src/main/java/com/example/techtron/firebaseauthdemo/ProfileActivity.java
+++ FirebaseAuthDemo/app/src/main/java/com/example/techtron/firebaseauthdemo/ProfileActivity.java
@@ -2,12 +2,34 @@ package com.example.techtron.firebaseauthdemo;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
-public class ProfileActivity extends AppCompatActivity {
+import com.google.firebase.auth.FirebaseAuth;
+
+public class ProfileActivity extends AppCompatActivity implements View.OnClickListener{
+
+    private FirebaseAuth firebaseAuth;
+
+    private TextView textViewUserEmail;
+    private Button buttonLogout;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_profile);
+
+        textViewUserEmail = (TextView) findViewById(R.id.textViewUserEmail);
+        buttonLogout = (Button) findViewById(R.id.buttonLogout);
+
+        buttonLogout.setOnClickListener(this);
+
+
+    }
+
+    @Override
+    public void onClick(View v) {
+
     }
 }
