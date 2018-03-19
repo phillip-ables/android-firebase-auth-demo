16b97381881c838fac8f552615c335fba4f41d72
Phillip <phillipables1@gmail.com>
2018-03-19 08:36:52 -0400

    declaired and initialized variables in out login activity

--- FirebaseAuthDemo/app/src/main/java/com/example/techtron/firebaseauthdemo/LoginActivity.java
+++ FirebaseAuthDemo/app/src/main/java/com/example/techtron/firebaseauthdemo/LoginActivity.java
@@ -2,12 +2,25 @@ package com.example.techtron.firebaseauthdemo;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class LoginActivity extends AppCompatActivity {
+
+    private Button buttonSignIn;
+    private EditText editTextEmail;
+    private EditText editTextPassword;
+    private TextView textViewSignin;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+
+        buttonSignIn = (Button) findViewById(R.id.buttonRegister);
+        editTextEmail = (EditText) findViewById(R.id.editTextEmail);
+        editTextPassword = (EditText) findViewById(R.id.editTextPassword);
+        textViewSignin = (TextView) findViewById(R.id.textViewSignin)
     }
 }
