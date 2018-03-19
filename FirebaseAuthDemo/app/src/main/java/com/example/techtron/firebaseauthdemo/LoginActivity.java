271f7b07d3ea1c4f13b79392feca52cfe579b3fe
Phillip <phillipables1@gmail.com>
2018-03-19 14:11:49 -0400

    if email or password is empty onclick then return with a toast instruction

--- FirebaseAuthDemo/app/src/main/java/com/example/techtron/firebaseauthdemo/LoginActivity.java
+++ FirebaseAuthDemo/app/src/main/java/com/example/techtron/firebaseauthdemo/LoginActivity.java
@@ -3,10 +3,12 @@ package com.example.techtron.firebaseauthdemo;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.text.TextUtils;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
 
 public class LoginActivity extends AppCompatActivity implements View.OnClickListener {
 
@@ -32,6 +34,17 @@ public class LoginActivity extends AppCompatActivity implements View.OnClickList
     private void userLogin(){
         String email = editTextEmail.getText().toString().trim();
         String password = editTextPassword.getText().toString().trim();
+
+        if(TextUtils.isEmpty(email)){
+            Toast.makeText(this, "Please enter email", Toast.LENGTH_LONG).show();
+            return;
+        }
+        if(TextUtils.isEmpty(password)){
+            Toast.makeText(this, "Please enter password", Toast.LENGTH_SHORT).show();
+            return;
+        }
+
+
     }
 
     @Override
