a77d92169d3001bf47628d338fc9e4063892834d
Phillip <phillipables1@gmail.com>
2018-03-19 08:32:34 -0400

    new login activity with ui

--- FirebaseAuthDemo/app/src/main/java/com/example/techtron/firebaseauthdemo/MainActivity.java
+++ FirebaseAuthDemo/app/src/main/java/com/example/techtron/firebaseauthdemo/MainActivity.java
@@ -82,7 +82,7 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
                             return;
                         }
                     }
-                })
+                });
 
     }
 
